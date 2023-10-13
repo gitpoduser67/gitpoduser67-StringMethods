6b95a168c6d382e67ce8a19ae0c06e52a2e43972
jhigherevature <71463449+jhigherevature@users.noreply.github.com>
2023-10-13 07:32:03 -0600

    Updating order of test arguments to match expected and actual values

--- src/test/java/SimpleStringMethodsTest.java
+++ src/test/java/SimpleStringMethodsTest.java
@@ -22,28 +22,28 @@ public class SimpleStringMethodsTest {
      */
     @Test
     public void charAtTest1(){
-        Assert.assertEquals(ssm.getCharacterAtIndex("abcdef", 2), 'c');
+        Assert.assertEquals('c',ssm.getCharacterAtIndex("abcdef", 2));
     }
     /**
      * the char at position 0 of 'abcdef' should return 'a'.
      */
     @Test
     public void charAtTest2(){
-        Assert.assertEquals(ssm.getCharacterAtIndex("abcdef", 0), 'a');
+        Assert.assertEquals('a', ssm.getCharacterAtIndex("abcdef", 0));
     }
     /**
      * the index of 'b' of "zebra" should return 2.
      */
     @Test
     public void indexOfTest1(){
-        Assert.assertEquals(ssm.getIndexGivenCharacter("zebra", 'b'), 2);
+        Assert.assertEquals(2, ssm.getIndexGivenCharacter("zebra", 'b'));
     }
     /**
      * the index of 'q' of "zebra" should return -1.
      */
     @Test
     public void indexOfTest2(){
-        Assert.assertEquals(ssm.getIndexGivenCharacter("banana", 'q'), -1);
+        Assert.assertEquals(-1, ssm.getIndexGivenCharacter("banana", 'q'));
     }
 
 }
